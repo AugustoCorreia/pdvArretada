763ab6173166ecf766dc84a560bfd0964abeba7d
Augusto Correia <augusto.correia@live.com>
2020-10-18 19:28:53 -0300

    teste1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'vender',
+    loadChildren: () => import('./pages/vender/vender.module').then( m => m.VenderPageModule)
   }
 ];
 @NgModule({
